c08572592e1e036e9fa6f3e226fbd7f41a13b941
Yurij <abc_z1@spaces.ru>
2024-09-24 14:17:56 +0300

    update

---
+++ tools/website_update.bat
@@ -0,0 +1,11 @@
+cd C:\Users\EliteBook\Desktop\georg\1Projects\websites\website
+git add .
+git commit -m "update"
+git push
+hugo -d C:\Users\EliteBook\Desktop\georg\1Projects\websites\yurijgrim.github.io
+cd C:\Users\EliteBook\Desktop\georg\1Projects\websites\yurijgrim.github.io
+git add .
+git commit -m "update"
+git push
+echo "Website updated -- ok"
+sleep 5
